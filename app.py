c3cda38d4e1b4b2cfa13d049e4b85f8b60811e54
Sam <samuel.hields@gmail.com>
2017-12-22 17:17:44 +0000

    Modified app and added run file to create DB tables in deployment

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'sam'
 api = Api(app)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) # /auth new end point
 
 api.add_resource(Store, '/store/<string:name>')
